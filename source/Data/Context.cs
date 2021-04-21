b05e78921feb83b6ff29c418994b60d711b6e9ea
louis.ribault <louis.ribault@epita.fr>
2021-04-21 16:43:37 +0100

    Login starting to advance decently

--- source/Data/Context.cs
+++ source/Data/Context.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Data
 {
-    public class Context : DbContext
+    public class Context : IdentityDbContext
     {
         public Context(DbContextOptions<Context> options) : base(options)
         {
 
         }
+
         public DbSet<Profile> Profile { get; set; }
         public DbSet<Student> Student { get; set; }
         public DbSet<Teacher> Teacher { get; set; }
