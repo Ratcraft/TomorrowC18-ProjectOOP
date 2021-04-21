d900b1116ac2fcd2a1cd67f3df0c301673ca1d0e
Zeryuix <leo.mermet91@gmail.com>
2021-04-21 17:10:58 +0100

    Modify Academic Calendar

--- source/Data/Context.cs
+++ source/Data/Context.cs
@@ -18,5 +18,6 @@ namespace Data
         public DbSet<Teacher> Teacher { get; set; }
         public DbSet<Admin> Admin { get; set; }
         public DbSet<AcademicCalendar> AcademicCalendar { get; set; }
+        public DbSet<CalendarEvent> CalendarEvent { get; set; }
     }
 }
---
+++ source/Migrations/20210421153956_Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TomorrowC18ProjectOOP.Migrations
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CalendarEvent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Start = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    End = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalendarEvent", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalendarEvent");
+        }
+    }
+}
