a284eead211c038ae4b6e543cd2a41710aefbae3
SwLina <tony.fiori@epita.fr>
2021-04-21 15:36:44 +0100

    Update Assignement.cs

--- source/Models/Assignement.cs
+++ source/Models/Assignement.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace TomorrowC18ProjectOOP.Models
 {
     public class Assignement
     {
+        [Key]
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public int studentId { get; set; }
+        [Required]
+        public string CourseName { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
     }
 }
