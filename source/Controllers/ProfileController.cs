a6a656e41d5127b32732a44ac61e70f8c7bcb4a8
louis.ribault <louis.ribault@epita.fr>
2021-05-21 14:04:12 +0200

    now profile working

---
+++ source/Controllers/ProfileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<Profile> userManager;
+
+        public ProfileController(UserManager<Profile> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            var userid = userManager.GetUserId(HttpContext.User);
+            Profile user = userManager.FindByIdAsync(userid).Result;
+            return View(user);
+        }
+    }
+}
